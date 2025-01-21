bceeaf08705312e0b5a29bb58b0af2dd9071b6e7
Alejandro <alejandrorcc24@gmail.com>
2025-01-21 13:30:09 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function()
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generateExcuse() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    function getRandomElement(array) {
+      let randomIndex = Math.floor(Math.random() * array.length);
+      return array[randomIndex];
+    }
+  
+    let randomWho = getRandomElement(who);
+    let randomAction = getRandomElement(action);
+    let randomWhat = getRandomElement(what);
+    let randomWhen = getRandomElement(when);
+  
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    return excuse;
+    document.querySelector("#excuse").innerHTML = excuse;
+    console.log("Hello Rigo from the console!");
 };
+
